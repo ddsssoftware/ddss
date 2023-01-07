d9736a1be1340a4abd12ba5e08079bfbf2f05132
ggmm <ddss.software@gmail.com>
2023-01-07 13:30:14 -0300

    Fix script trigger

--- resources/views/case/description.blade.php
+++ resources/views/case/description.blade.php
@@ -2,9 +2,10 @@
     <h3>{{ __('ddss.case_description_title') }}</h3>
     <form id="case_description_form"
           method="POST"
-          action="{{ route('case.updatedescription') }}"
-          data-form-needs-case="true">
+          action="{{ route('case.updatedescription') }}">
         <textarea id="case_description_form_text" name="description">{{ ($case['description'] ?? '') }}</textarea>
-        <button id="case_description_form_submit">{{ __('ddss.case_description_form_save') }}</button>
+        <button id="case_description_form_submit"
+                type="button"
+                data-form-needs-case="true">{{ __('ddss.case_description_form_save') }}</button>
     </form>
 </section>
--- resources/views/conditions/search.blade.php
+++ resources/views/conditions/search.blade.php
@@ -1,8 +1,11 @@
 <section id="conditions_search">
     <h3>{{ __('ddss.conditions_search_title') }}</h3>
-    <form id="conditions_search_form" method="POST" action="{{ route('case.condition.search') }}">
+    <form id="conditions_search_form"
+          method="POST"
+          action="{{ route('case.condition.search') }}"
+          data-form-needs-case="true">
         <input name="term" id="conditions_search_form_text">
-        <button id="conditions_search_form_search">{{ __('ddss.conditions_search_form_search') }}</button>
+        <button id="conditions_search_form_submit" type="button">{{ __('ddss.conditions_search_form_search') }}</button>
     </form>
     @if(isset($conditionSearchResult))
         @each('conditions.item', $conditionSearchResult, 'result', 'conditions.noitem')
