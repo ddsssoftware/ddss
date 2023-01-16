623bbf7c3ab226bd1636884c77c55fe1f6ede5a3
ggmm <ddss.software@gmail.com>
2023-01-15 21:26:05 -0300

    Remove auth app service

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -18,10 +18,7 @@
  */
 namespace App\Providers;
 
-use Illuminate\Cache\RateLimiting\Limit;
 use Illuminate\Foundation\Support\Providers\RouteServiceProvider as ServiceProvider;
-use Illuminate\Http\Request;
-use Illuminate\Support\Facades\RateLimiter;
 use Illuminate\Support\Facades\Route;
 
 class RouteServiceProvider extends ServiceProvider
@@ -29,9 +26,7 @@ class RouteServiceProvider extends ServiceProvider
     public function boot()
     {
         $this->routes(function () {
-            Route::
-                namespace($this->namespace)
-                ->group(base_path('routes/web.php'));
+            Route::namespace($this->namespace)->group(base_path('routes/web.php'));
         });
     }
 }
--- config/app.php
+++ config/app.php
@@ -52,8 +52,6 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
 
-        App\Providers\AppServiceProvider::class,
-        App\Providers\AuthServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
 
     ],
