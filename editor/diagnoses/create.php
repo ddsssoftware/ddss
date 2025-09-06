ecef1e6717478db9f928f3241ac74009014f810f
ddsssoftware <ddss.software@gmail.com>
2025-09-06 09:14:42 -0400

    add reset button

--- editor/diagnoses/create.php
+++ editor/diagnoses/create.php
@@ -8,6 +8,7 @@ include_once __DIR__ . '/../inc/data.php';
     <?php include_once __DIR__ . '/form.php'; ?>
     <div class="field">
         <button class="button is-link">Save</button>
+        <a href="" class="button is-danger has-text-primary-100">Reset</a>
     </div>
 </form>
 
--- editor/symptoms/edit.php
+++ editor/symptoms/edit.php
@@ -9,6 +9,7 @@ $symptom = getSymptomById($_GET['id'])['value'];
     <?php include_once __DIR__ . '/form.php'; ?>
     <div class="field">
         <button class="button is-link">Save</button>
+        <a href="" class="button is-danger has-text-primary-100">Reset</a>
     </div>
 </form>
 
