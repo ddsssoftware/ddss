5dbb5d258db87dcd1254bc5f4124bc446aff664b
ddsssoftware <ddss.software@gmail.com>
2025-11-23 14:50:27 -0500

    sonar

--- editor/symptoms/form.php
+++ editor/symptoms/form.php
@@ -1,21 +1,23 @@
 <input class="input" type="hidden" name="id" value="<?= isset($symptom) ? $symptom['id'] : '' ?>">
 <div class="field">
-    <label class="label">ID</label>
+    <label class="label" for="id">ID</label>
     <div class="control">
-        <input class="input" type="text" name="id" value="<?= isset($symptom) ? $symptom['id'] : '' ?>" disabled>
+        <input class="input" type="text" id="id" name="id" value="<?= isset($symptom) ? $symptom['id'] : '' ?>" disabled>
     </div>
 </div>
 <div class="field">
-    <label class="label">Name</label>
+    <label class="label" for="name">Name</label>
     <div class="control">
-        <input class="input" type="text" name="name" value="<?= isset($symptom) ? $symptom['name'] : '' ?>" required>
+        <input class="input" type="text" id="name" name="name" value="<?= isset($symptom) ? $symptom['name'] : '' ?>" required>
     </div>
 </div>
 <div class="field pb-6">
     <span class="label">Associate with diagnosis</span>
     <div class="control radios is-block">
-        <?php foreach (array_slice($diagnoses, -5, 5) as $d)
+        <?php
+        foreach (array_slice($diagnoses, -5, 5) as $d) {
             echo '<label class="radio is-block pl-3"><input type="radio" class="mr-3" name="diagnosisid" value="' . $d['id'] . '" />' . $d['name'] . '</label>' . PHP_EOL;
+        }
         ?>
     </div>
 </div>
