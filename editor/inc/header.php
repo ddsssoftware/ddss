a4376c4372c2a17f6e84016c9075803aaa435b02
ddsssoftware <ddss.software@gmail.com>
2025-11-23 14:42:20 -0500

    sonar

--- editor/inc/header.php
+++ editor/inc/header.php
@@ -1,7 +1,8 @@
 <!DOCTYPE html>
-<html>
+<html lang="en">
 
 <head>
+    <title>DDSS</title>
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@1.0.4/css/bulma.min.css">
     <link href="https://cdn.jsdelivr.net/npm/gridjs/dist/theme/mermaid.min.css" rel="stylesheet" />
