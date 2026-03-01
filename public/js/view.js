92638243db5908a53b49a5cc36771c9f2a27fbda
ddsssoftware <ddss.software@gmail.com>
2026-03-01 16:30:13 -0300

    symptom search

--- public/js/view.js
+++ public/js/view.js
@@ -1,7 +1,24 @@
+// #region SymptomsSearchComponent
 const SymptomsSearchComponent = function() {
+    return {
+        search(event) {
+            const term = event.target.value;
+            let symptoms = [];
+            if (term.length > 1) {
+                symptoms = Search.symptoms(term);
+            }
+            this.fill(symptoms);
+        },
 
+        fill(symptoms) {
+            
+        }
+    }
 }();
+// #endregion
 
+// #region DiagnosesSearchComponent
 const DiagnosesSearchComponent = function() {
 
 }();
+// #endregion
