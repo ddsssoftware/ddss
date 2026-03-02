0310c2216272245a707d414791e2c2b28da244d8
ddsssoftware <ddss.software@gmail.com>
2026-03-02 09:36:04 -0300

    draft symptom item

--- public/js/view.js
+++ public/js/view.js
@@ -1,5 +1,6 @@
 // #region SymptomsSearchComponent
-const SymptomsSearchComponent = function() {
+const SymptomsSearchComponent = function () {
+    const SYMPTOMS_RESULTS_ID = symptoms__datalist_search;
     return {
         search(event) {
             const term = event.target.value;
@@ -11,14 +12,34 @@ const SymptomsSearchComponent = function() {
         },
 
         fill(symptoms) {
-            
+            const $resultsDiv = document.getElementById(SYMPTOMS_RESULTS_ID);
         }
     }
 }();
 // #endregion
 
+// #region SymptomItem
+class SymptomItem extends HTMLElement {
+
+    constructor() {
+        super();
+    }
+
+    connectedCallback() {
+        const root = this.attachShadow({ mode: 'closed' });
+        root.innerHTML = `
+            <button hx-get="/my-component-clicked" hx-target="next div">Click me!</button>
+            <div></div>
+            `;
+        htmx.process(root);
+    }
+}
+
+customElements.define('symptom-item', SymptomItem);
+// #endregion
+
 // #region DiagnosesSearchComponent
-const DiagnosesSearchComponent = function() {
+const DiagnosesSearchComponent = function () {
 
 }();
 // #endregion
